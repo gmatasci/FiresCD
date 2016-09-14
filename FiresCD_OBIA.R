e0e28690e80ae6e1351bea5ab6b2c118ba7c64d9
Giona Matasci <giona.matasci@gmail.com>
2016-09-14 16:17:30 -0700

    14/09/2016 Before changes to integrate single and multi scale

--- FiresCD_OBIA.R
+++ FiresCD_OBIA.R
@@ -28,7 +28,7 @@
 
 #### INIT --------------------------------------------------------------------
 
-print('OBIA for fires CD') 
+print('OBIA for fires CD')
 
 rm(list=ls())
 
@@ -47,33 +47,39 @@ params$approach <- list("PixelBased", "ObjectSingleScale", "ObjectMultiScale")  
                                                       ## ObjectSingleScale --> optimizes the parameters of a Meanshift segmentation with an object-based approach  
                                                       ## ObjectMultiScale --> features computes for different segmentations are stacked in the same df with a pixel-based approach
 # params$fires <- list("Liege")
-params$fires <- list("Tanghe", "Liege")  ## fires to consider
+params$fires <- list("Liege", "Levellers")
+# params$fires <- list("Abraham", "Cone", "Flett", "Keane", "Leggo", "Levellers", "Liege",
+#                      "Mcarther", "Overflow", "Perry", "Rail", "Rainbow", "Steephill", "Tanghe")  ## fires to consider
+# params$fires <- list("Liege", "Rail", "Tanghe")  ## fires to consider
+
 params$subsetting <- T    ## to subset data for development purposes (still to implement)
 params$mort.classes <- c("0-5%", "6-25%", "95-100%")
 params$critical.class <- "Class: 6-25%"  ## to compute F-measure as an alternative, more detailed measure to Kappa
 
 ## Meanshift
-params$meanshift.testmode <- F    ## if set to T saves each segmentation result (shp) with parameter values to visually inspect the segments
+params$nr.bands.seg <- 3
+params$meanshift.testmode <- T    ## if set to T saves each segmentation result (shp) with parameter values to visually inspect the segments
 # params$ranger.vect <- c(10, 50, 200) ## On SF imagebest with 50
 # params$spatialr.vect <- c(5, 20 , 50)  ## best with 5  
 # params$minsize.vect <- c(10, 100, 1000) ## best with 100
 # params$ranger.vect <- c(10, 20, 200, 500) ## On SF imagebest with 50
 # params$spatialr.vect <- c(3, 10, 20, 50)  ## best with 5
 # params$minsize.vect <- c(10) ## best with 100
-params$ranger.vect <- c(5, 20) ## Range radius: on SF imagebest with 50, Liege with 25
-params$spatialr.vect <- c(5, 20)  ## Spatial radius: on SF best with 5, Liege with 5
+params$ranger.vect <- c(50, 100, 200, 300) ## Range radius: on SF imagebest with 50, Liege with 25
+params$spatialr.vect <- c(10)  ## Spatial radius: on SF best with 5, Liege with 5
 params$minsize.vect <- c(5) ## Minimum object size: on SF best with 100, for fireswith 5, setting it to 0 just produces 1-pixel segments
+params$multisc.params <- data.frame(ranger=c(5, 20), spatialr=c(5, 20), minsize=c(5, 5))
 
 ## RF 
 params$base.predictors <- c("db1", "db2", "db3", "db4", "db5", "db7", 
                                "dNBR", "dTCG", "dTCB", "dTCW", "dNDVI", "dNDWI", 
-                               "elev", "slope", "aspect", 
+                               "elev", "slope", "aspect.sin", "aspect.cos", 
                                "EOSD")   ## list of starting predictors (for the classification part, as the segmentation is run on 6-band difference image only)
 params$obj.predictors <- c("db1_mean", "db1_sd", "db2_mean", "db2_sd", "db3_mean", "db3_sd", "db4_mean", "db4_sd", "db5_mean", "db5_sd", "db7_mean", "db7_sd", 
                               "dNBR_mean", "dNBR_sd", "dTCG_mean", "dTCG_sd", "dTCB_mean", "dTCB_sd", "dTCW_mean", "dTCW_sd", "dNDVI_mean", "dNDVI_sd", "dNDWI_mean", "dNDWI_sd",
-                              "elev_mean", "elev_sd", "slope_mean", "slope_sd", "aspect_mean", "aspect_sd",
-                              "EOSD", "EOSD_majpct",
-                              "nrpixseg")   ## list of final predictors computed at the object level
+                              "elev_mean", "elev_sd", "slope_mean", "slope_sd", "aspect.sin_mean", "aspect.sin_sd", "aspect.cos_mean", "aspect.cos_sd",
+                              "EOSD_maj", "EOSD_majpct",
+                              "nrpixseg")    ## list of final predictors computed at the object level
 # params$metric <- "Kappa"   ## not implemented yet
 # params$metric <- "MixKappaFm"  
 params$targ <- "CLASS"     ## target variable of the classification (column name of dataframe)
@@ -131,6 +137,7 @@ for (pack in list.of.packages){
 meanShiftOTB <- function(ranger, spatialr, minsize, OTB.dir, OGR.dir, data.dir, temp.dir, img.file.name, seg.file.name, test.mode) {
   
   img.file <- file.path(data.dir, img.file.name)   ## image to segment
+  clean.img.file <- file.path(temp.dir, sprintf("ZeroPadded_%s", img.file.name))
   segment.file.sqlt <- file.path(temp.dir, sprintf("%s.sqlite", seg.file.name))  ## initial segmentation result as a sqlite file (only way to work with Lambert conformal conic projection)
   segment.file <- file.path(temp.dir, sprintf("%s.shp", seg.file.name))  ## segmentation result converted to shp via ogr2ogr
   
@@ -138,12 +145,21 @@ meanShiftOTB <- function(ranger, spatialr, minsize, OTB.dir, OGR.dir, data.dir, 
   if (file.exists(segment.file)){ 
     unlink(segment.file, recursive = T, force = T) 
     unlink(segment.file.sqlt, recursive = T, force = T)
-  } 
+  }
   
-  ## create text for command to run OTB (equivalent to saving this text in a batch file .bat and running it with system2() )
+  ## change no-data values to 0 to avoid the 1-segment-per-pixel situation outside the image (and get only a big polygon outside)
+  command.text <- sprintf("%s -in %s -out %s -mode changevalue -mode.changevalue.newv -0",  ## create text for command to run OTB (equivalent to saving this text in a batch file .bat and running it with system2() )
+                          file.path(OTB.dir, "otbcli_ManageNoData"),   ## complete path to OTB bat file doing the value change
+                          img.file,
+                          clean.img.file
+  )
+  system(command.text)  ## run command
+  
+  
+  ## actual segmentation
   command.text <- sprintf("%s -in %s -filter meanshift -filter.meanshift.ranger %d -filter.meanshift.spatialr %d -filter.meanshift.minsize %d -mode vector -mode.vector.out %s -mode.vector.layername %s & %s %s %s", 
                       file.path(OTB.dir, "otbcli_Segmentation"),   ## complete path to OTB bat file doing the segmentation
-                      img.file,
+                      clean.img.file,
                       ranger, 
                       spatialr,
                       minsize,
@@ -187,6 +203,12 @@ fires.classif.metrics <- function(predicted, observed, critical.class) {
 tic <- proc.time() ## start clocking global time
 
 load(file.path(data.dir, "allpixels.Rdata"))
+allpixels <- dfp
+rm(dfp)
+
+allpixels <- allpixels %>% 
+             filter(NAME %in% toupper(params$fires)) %>%   ## to keep only fires actually specified in params 
+             select_(.dots = c("NAME", "CLASS", "X", "Y", params$base.predictors))  ## keep only predictors of interest
 
 #### LOOCV OVER FIRES ---------------------------------------------------
 
@@ -201,7 +223,7 @@ for (fire.out in params$fires) {  ## LOO-CV loop over the fires to leave out
   allpixels.in <- allpixels[idx.pix.in,]   ## df with pixels in
   allpixels.out <- allpixels[idx.pix.out,]  ## df with pixels out
 
-#### SEGMENTATION PARAMETERS OPTIMIZATION --------------------------------
+#### SEGMENTATION OF KEPT-IN FIRES --------------------------------------
 
   multi.scale.pix.df <- data.frame(matrix(nrow=0, ncol=0))   ## stil to implement
   OAs.OOB.meanshift <- data.frame(matrix(nrow=0, ncol=4))   ## initialize empty matrix to store Out-of-Bag Overall Accuracies and associated parameters for each segmentation
@@ -211,8 +233,9 @@ for (fire.out in params$fires) {  ## LOO-CV loop over the fires to leave out
   ms.par.col <- 1  ## start at 1 the index over columns of dataframe of segment IDs
   
   ## fill object level df with per segment mean and std dev
-  multi.scale.pix.dt <- data.table()  ## initialize dt to store object-level features for all the kept-in pixels
-  
+  multi.scale.pix.in.dt <- data.table(allpixels.in[, c("NAME", "CLASS", params$base.predictors )])  ## initialize dt to store object-level features for all the kept-in pixels
+  multisc.idx <- 1  ## index iterating over the sets of multiscale parameters
+
   ## 3 nested loops over the Meanshift parameters
   for (ranger in params$ranger.vect) {
     for (spatialr in params$spatialr.vect) {
@@ -220,14 +243,15 @@ for (fire.out in params$fires) {  ## LOO-CV loop over the fires to leave out
         
         ## loop to segment all the fires kept in
         for (fire.in in fires.in) {
-          
-          img.file.name <- sprintf("%s_6bands.tif", fire.in)
+
+          img.file.name <- sprintf("%s_%sbands.tif", fire.in, params$nr.bands.seg)
+
           if (params$meanshift.testmode) {  ## if TRUE save segmentation result with a telling name 
             seg.file.name <- sprintf("Segment_%s_ranger%d_spatial%d_minsize%d", fire.in, ranger, spatialr, minsize)
           } else {   ## otherwise just overwrite it in a file called Segment
             seg.file.name <- "Segment"
           }
-          segments <- meanShiftOTB(ranger, spatialr, minsize, OTB.dir, OGR.dir, data.dir, temp.dir, img.file.name, seg.file.name, params$meanshift.testmode)  ## main Meanshift segmentation
+          segments <- meanShiftOTB(ranger, spatialr, minsize, OTB.dir, OGR.dir, file.path(data.dir, sprintf("%s_bands", params$nr.bands.seg)), temp.dir, img.file.name, seg.file.name, params$meanshift.testmode)  ## main Meanshift segmentation
 
           fire.image.proj <- CRS(proj4string(segments))   ## get projection string from segments
           
@@ -275,10 +299,20 @@ for (fire.out in params$fires) {  ## LOO-CV loop over the fires to leave out
         colnames(single.scale.obj.df)[colnames(single.scale.obj.df) == "CLASS_npixel"] <- "CLASS_majpct"  ## ...the ground truth...
         single.scale.obj.df$EOSD_npixel <- single.scale.obj.df$EOSD_npixel / single.scale.obj.df$nrpixseg
         colnames(single.scale.obj.df)[colnames(single.scale.obj.df) == "EOSD_npixel"] <- "EOSD_majpct"  ## ...and the EOSD layer
+        colnames(single.scale.obj.df)[colnames(single.scale.obj.df) == "EOSD"] <- "EOSD_maj"  ## ...and the EOSD layer
         
-        ## if segmentation level is among a specified set, store for every pixel the summarized predictor values at the object level
-        
-        data.table(single.scale.obj.df"NAME", "segID"
+        ## if segmentation scale is among the set specified by params$multisc.params, store for every pixel the summarized predictor values at the object level
+        if (ranger %in% params$multisc.params$ranger[multisc.idx] & 
+            spatialr %in% params$multisc.params$spatialr[multisc.idx] & 
+            minsize %in% params$multisc.params$minsize[multisc.idx]) {
+          single.scale.obj.dt <- data.table(single.scale.obj.df, key=c("NAME", "segID"))
+          multi.scale.pix.in.dt[, segID:=allpixels.in$segID]
+          setkey(multi.scale.pix.in.dt, NAME, segID)
+          cols <- c("NAME", "segID", params$obj.predictors)  ## colnames of columns to be joined
+          multi.scale.pix.in.dt <- multi.scale.pix.in.dt[single.scale.obj.dt[, cols, with=F]]   ## merge data.tables by the defined keys (NAME and segID)
+          setnames(multi.scale.pix.in.dt, old=params$obj.predictors, new=sprintf("Sc%s_%s", multisc.idx, params$obj.predictors))
+          multisc.idx <- multisc.idx + 1  
+        }
         
         ## set mtry parameter according to params$mtry
         nr.vars <- length(params$obj.predictors) 
@@ -289,8 +323,8 @@ for (fire.out in params$fires) {  ## LOO-CV loop over the fires to leave out
         }
         set.seed(params$seed)   
         ## apply RF on df with object-level values using as predictors the columns listed in params$obj.predictors and with response variable the column specified in params$targ
-        RF <- randomForest(x=single.scale.obj.df[,params$obj.predictors], y=single.scale.obj.df[,params$targ], ntree=params$ntree, mtry=mtries, nodesize=params$nodesize, importance=params$plot.importance)
-        OAs.OOB.meanshift <- rbind( OAs.OOB.meanshift, data.frame(ranger, spatialr, minsize, OA=1-RF$err.rate[params$ntree, "OOB"]) )   ## grow df with results
+        # RF <- randomForest(x=single.scale.obj.df[,params$obj.predictors], y=single.scale.obj.df[,params$targ], ntree=params$ntree, mtry=mtries, nodesize=params$nodesize, importance=params$plot.importance)
+        # OAs.OOB.meanshift <- rbind( OAs.OOB.meanshift, data.frame(ranger, spatialr, minsize, OA=1-RF$err.rate[params$ntree, "OOB"]) )   ## grow df with results
       
         ms.par.col <- ms.par.col+1  ## increment index over columns of dataframe of IDs
         
@@ -300,21 +334,33 @@ for (fire.out in params$fires) {  ## LOO-CV loop over the fires to leave out
   
   OAs.OOB.meanshift.sorted <- arrange(OAs.OOB.meanshift, desc(OA))   ## sort df with results by decreasing OA
   
-#### SEGMENTATION WITH BEST PARAMETERS ---------------------------------------------
+#### SEGMENTATION OF LEFT-OUT FIRE ---------------------------------------------
   
-  ## segment left out fire with best parameters
-  img.file.name <- sprintf("%s_6bands.tif", fire.out)
-  if (params$meanshift.testmode) {
-    seg.file.name <- sprintf("Segment_%s_ranger%d_spatial%d_minsize%d", fire.out, OAs.OOB.meanshift.sorted$ranger[1], OAs.OOB.meanshift.sorted$spatialr[1], OAs.OOB.meanshift.sorted$minsize[1])
-  } else {
-    seg.file.name <- "Segment"
+  ## segment the left-out fire with the multiscale parameters and, if not already included, with the best parameters
+  ranger.vect.LO <- unique(c(params$ranger.vect.multisc, OAs.OOB.meanshift.sorted$ranger[1]))
+  spatialr.vect.LO <- unique(c(params$spatialr.vect.multisc, OAs.OOB.meanshift.sorted$spatialr[1]))
+  minsize.vect.LO <- unique(c(params$minsize.vect.multisc, OAs.OOB.meanshift.sorted$minsize[1]))
+  
+  for (ranger in ranger.vect.LO) {
+    for (spatialr in spatialr.vect.LO) {
+      for (minsize in minsize.vect.LO) {
+        ## segment left out fire with best parameters
+        img.file.name <- sprintf("%s_%sbands.tif", fire.out, params$nr.bands.seg)
+        if (params$meanshift.testmode) {
+          seg.file.name <- sprintf("Segment_%s_ranger%d_spatial%d_minsize%d", fire.out, ranger, spatialr, minsize)
+        } else {
+          seg.file.name <- "Segment"
+        }
+        segments <- meanShiftOTB(ranger, spatialr, minsize, OTB.dir, OGR.dir, file.path(data.dir, sprintf("%s_bands", params$nr.bands.seg)), temp.dir, img.file.name, seg.file.name, params$meanshift.testmode)
+        fire.image.proj <- CRS(proj4string(segments)) 
+        coords <- SpatialPoints(allpixels.out[, c("X", "Y")], proj4string=fire.image.proj)  ## create spatial points for extrating segments from OTB
+        id.segment.out <- over(coords, segments)$dn
+        allpixels.out$segID <- id.segment.out  ## add segment IDs to the df with the left-out pixels
+        allpixels.out$npixel <- 1   ## add column of ones to count nr of pixels per segment
+        
+      }
+    }
   }
-  segments <- meanShiftOTB(OAs.OOB.meanshift.sorted$ranger[1], OAs.OOB.meanshift.sorted$spatialr[1], OAs.OOB.meanshift.sorted$minsize[1], OTB.dir, OGR.dir, data.dir, temp.dir, img.file.name, seg.file.name, params$meanshift.testmode)
-  fire.image.proj <- CRS(proj4string(segments)) 
-  coords <- SpatialPoints(allpixels.out[, c("X", "Y")], proj4string=fire.image.proj)  ## create spatial points for extrating segments from OTB
-  id.segment.out <- over(coords, segments)$dn
-  allpixels.out$segID <- id.segment.out  ## add segment IDs to the df with the left-out pixels
-  allpixels.out$npixel <- 1   ## add column of ones to count nr of pixels per segment
   
   ## retrieve segment IDs for kept-in fires associated with the best combination of parameters
   best.colname <- sprintf("rr%ssr%sms%s", OAs.OOB.meanshift.sorted$ranger[1], OAs.OOB.meanshift.sorted$spatialr[1], OAs.OOB.meanshift.sorted$minsize[1])
